6480c20ec736137abaff038bf93037adcd1f8230
IgnacioArias32 <igna.arias@gmail.com>
2021-05-26 10:10:19 -0300

    Cambios en el menu de inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,16 +28,13 @@
     ["productos", "ventas", "vender", "clientes"],
     ["usuarios"]
     ] as $modulos)
-        <div class="col-12 pb-2">
+        <div class="col-10 pb-2">
             <div class="row">
                 @foreach($modulos as $modulo)
-                    <div class="col-12 col-md-3">
+                    <div class="col-10 col-md-3">
                         <div class="card">
-                            <img class="card-img-top" src="{{url("/img/$modulo.png")}}">
+                            <img style="max-width:100%;width:auto;height:auto;" class="card-img-top" src="{{url("/img/$modulo.png")}}">
                             <div class="card-body">
-                                <h5 class="card-title">
-                                    {{$modulo === "acerca_de" ? "Acerca de" : ucwords($modulo)}}
-                                </h5>
                                 <a href="{{route("$modulo.index")}}" class="btn btn-success">
                                     Ir a&nbsp;{{$modulo === "acerca_de" ? "Acerca de" : ucwords($modulo)}}
                                     <i class="fa fa-arrow-right"></i>
