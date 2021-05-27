54f01ae9e5a85f2e4eeb27d477cc4841fff12a9f
IgnacioArias32 <igna.arias@gmail.com>
2021-05-27 16:43:38 -0300

    No puede ser 0 la cantidad

--- resources/views/vender/vender.blade.php
+++ resources/views/vender/vender.blade.php
@@ -48,10 +48,10 @@
                     @csrf
                     <div class="form-group">
                         <label for="cantidad">Cantidad</label>
-                        <input type="text" name="cantidad" autocomplete="off" id="cantidad" class="form-control"required autofocus name="cantidad" placeholder="Cantidad" />                    
+                        <input type="number" min="1" name="cantidad" autocomplete="off" id="cantidad" class="form-control"required autofocus name="cantidad" placeholder="Cantidad" />                    
                     </div>
                     {{ csrf_field() }}
-                    <button type="submit" class="btn btn-default">Agregar Producto
+                    <button type="submit" class="btn btn-primary">Agregar Producto &nbsp;
                     <i class="fa fa-plus-square fa-2x" aria-hidden="true"></i>                                        </button>
                 </form>
                 </div>
