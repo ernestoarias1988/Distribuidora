0a35003fec7e01e89fa9db153be667262eaaf696
IgnacioArias32 <igna.arias@gmail.com>
2021-05-30 23:05:48 -0300

    Stock disponible en producto seleccionado

--- routes/web.php
+++ routes/web.php
@@ -53,4 +53,5 @@ Route::middleware("auth")
         
         Route::post('/autocomplete/fetch', 'VenderController@fetch')->name('autocomplete.fetch');
         Route::post('/autocomplete/fetchcliente', 'VenderController@fetchcliente')->name('autocomplete.fetchcliente');
+        Route::post('/autocomplete/fetchcantidad', 'VenderController@fetchcantidad')->name('autocomplete.fetchcantidad');
     });
