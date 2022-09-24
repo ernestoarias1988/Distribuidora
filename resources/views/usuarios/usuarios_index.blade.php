66d11dc294633f179cbfe04131eba1914ebb6594
Ignacio <igna.arias@gmail.com>
2022-09-24 11:16:13 -0300

    Disable Edit and Delete Admin User

--- resources/views/usuarios/usuarios_index.blade.php
+++ resources/views/usuarios/usuarios_index.blade.php
@@ -1,14 +1,14 @@
 @extends("maestra")
 @section("titulo", "Usuarios")
 @section("contenido")
-    <div class="row">
-        <div class="col-12">
-            <h1>Usuarios <i class="fa fa-users"></i></h1>
-            <a href="{{route("usuarios.create")}}" class="btn btn-success mb-2">Agregar</a>
-            @include("notificacion")
-            <div class="table-responsive">
-                <table class="table table-bordered">
-                    <thead>
+<div class="row">
+    <div class="col-12">
+        <h1>Usuarios <i class="fa fa-users"></i></h1>
+        <a href="{{route("usuarios.create")}}" class="btn btn-success mb-2">Agregar</a>
+        @include("notificacion")
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
                     <tr>
                         <th>Usuario</th>
                         <th>Nombre</th>
@@ -16,32 +16,36 @@
                         <th>Editar</th>
                         <th>Eliminar</th>
                     </tr>
-                    </thead>
-                    <tbody>
+                </thead>
+                <tbody>
                     @foreach($usuarios as $usuario)
-                        <tr>
+                    <tr>
                         <td><a href="{{route('totales.index', ["vendedor"=>$usuario->email])}}"> {{$usuario->email}}</a></td>
-                            <td>{{$usuario->name}}</td>
-                            <td>{{$usuario->role_id}}</td>
-                            <td>
-                                <a class="btn btn-warning" href="{{route("usuarios.edit",[$usuario])}}">
-                                    <i class="fa fa-edit"></i>
-                                </a>
-                            </td>
-                            <td>
-                                <form action="{{route("usuarios.destroy", [$usuario])}}" method="post">
-                                    @method("delete")
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger">
-                                        <i class="fa fa-trash"></i>
-                                    </button>
-                                </form>
-                            </td>
-                        </tr>
+                        <td>{{$usuario->name}}</td>
+                        <td>{{$usuario->role_id}}</td>
+                        <td>
+                            @if($usuario->name!='Administrador')
+                            <a class="btn btn-warning" href="{{route("usuarios.edit",[$usuario])}}">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                            @endif
+                        </td>
+                        <td>
+                            @if($usuario->name!='Administrador')
+                            <form action="{{route("usuarios.destroy", [$usuario])}}" method="post">
+                                @method("delete")
+                                @csrf
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                            @endif
+                        </td>
+                    </tr>
                     @endforeach
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 @endsection
